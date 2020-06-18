e3a957510e586c033ed575939f0fa3b4b11d15ff
alozano-77 <53537632+alozano-77@users.noreply.github.com>
2020-06-18 02:44:03 -0700

    AGTS modified

--- README.md
+++ README.md
@@ -1,2 +1,59 @@
-# html-s3-blog
-SEO HTML blog hosted on AWS S3
+# A Girl's Travel Secret (AGTS)
+
+This is a modified version of Editorial, originally created by [@ajlkn](https://twitter.com/ajlkn) for [HTML5 UP](https://html5up.net).
+These some modifications to improve SEO and ease of use.
+
+**Demo:** <https://www.agirlstravelsecrets.com>
+
+## Additional features included
+
+### Site Search Integration
+
+- [jQuery plugin](https://tipue.com/search)
+  - No need to use Google provided search
+  - Modify `tipuesearch/tipuesearch_content.js`
+
+### Lazy Loaded Images
+
+Images are provided via [lazysizes](https://github.com/aFarkas/lazysizes) using the following code:
+
+```html
+<img
+ alt=""
+  data-sizes="auto"
+  data-srcset="small.jpg 500w,
+    medium.jpg 640w,
+    big.jpg 1024w"
+  data-src="medium"
+  class="lazyload" />
+```
+
+### Google Analytics Integration
+
+Replace the code in `<head>` with the code from Google Analytics.
+
+### Additional Files Needed
+
+To maintain a high SEO, I suggest you include the following files:
+
+- `favicon.ico` - [fiverr](https://www.fiverr.com) was great for this
+- `robots.txt` - Modified from [reddit](https://www.reddit.com/robots.txt)
+- `sitemap.xml` - Can be created [here](www.xml-sitemaps.com)
+
+## AWS Integration
+
+### Route53 and S3
+
+This can be hosted on an S3 bucket using [AWS static website hosting](https://docs.aws.amazon.com/AmazonS3/latest/dev/WebsiteHosting.html).
+
+### CloudFront Training
+
+Additional training on integrating CloudFront can be found from the [SAA-C02 course](https://learn.cantrill.io) from Adrian Cantrill.
+
+### Efficient Image Handling
+
+This was achieved using the [serverless image handler](https://github.com/awslabs/serverless-image-handler) provided from AWS Labs.
+
+## Copyright & License
+
+Copyright (c) 2013-2020 [HTML5 UP](https://htmlup.net) & [A Girl's Travel Secrets](https://www.agirlstravelsecrets.com) - This theme is licensed under both the [MIT and Creative Commons Attribution 3.0](LICENSE). Please note that the terms of the Creative Commons license require that you maintain the footer attribution to freely use this theme.
---
+++ tipuesearch/tipuesearch_content.js
@@ -0,0 +1,18 @@
+
+var tipuesearch = {
+     "pages": [
+          { "title": "Homepage", "url": "https://www.agirlstravelsecrets.com/index.html", "text": "Join our travels and find somewhere new to explore." },
+          { "title": "Barcelona", "url": "https://www.agirlstravelsecrets.com/blog/barcelona.html", "text": "Barcelona is the life of the party in Spain. You can find great bars, beaches, restaurants and art from the famous artist Gaudi, sprinkled around the city." },
+          { "title": "Florence", "url": "https://www.agirlstravelsecrets.com/blog/florence.html", "text": "Florence is home of the most mouth watering steak you’ll ever eat. You’ll also find rare architecture, the tombs of the famous artists Michael Angelo, Machiavelli and Galileo." },
+          { "title": "Naples", "url": "https://www.agirlstravelsecrets.com/blog/naples.html", "text": "Naples is the entry way to picture perfect islands off the coast of Italy but it’s also the home of the best Pizza in the world. Positano is the most beautiful town you’ll ever visit, baming with color and beach vibes." },
+          { "title": "Rome", "url": "https://www.agirlstravelsecrets.com/blog/rome.html", "text": "History is embedded in the streets of Rome. You’ll be overwhelmed with awe when you visit the Colosseum, Roman Forum, and Trevi Fountain. You also can’t deny that Italy serves the absolute best pasta and gelato of all time." },
+          { "title": "Disneyland", "url": "https://www.agirlstravelsecrets.com/blog/disneyland.html", "text": "Disneyland is the ultimate playground for people of all ages. You’ll be reminded of your favorite childhood characters and the movies you once watched over and over, will come to life in the parades, rides and characters walking around the park." },
+          { "title": "Flights", "url": "https://www.agirlstravelsecrets.com/blog/flights.html", "text": "Flights can seem expensive, but you will learn to book like a pro!", "tags": "blog" },
+          { "title": "Madrid", "url": "https://www.agirlstravelsecrets.com/blog/madrid.html", "text": "If you’re looking to feed your soul with unbelievable tapas, drink sangria, and shop till you drop, Madrid is perfect. You’ll also find wonderful architecture, history of the Royal families of Spain on every corner." },
+          { "title": "Packing", "url": "https://www.agirlstravelsecrets.com/blog/packing.html", "text": "Putting a packing list can seem daunting. I can show you what not to leave home without.", "tags": "blog" },
+          { "title": "Paris", "url": "https://www.agirlstravelsecrets.com/blog/paris.html", "text": "The city of love is full of history, carbs and beautiful views. The Eiffel Tower is the crown jewel of the city but you can also find places like the Louvre Museum, cathedrals and more that you’ll absolutely love. " },
+          { "title": "Planning", "url": "https://www.agirlstravelsecrets.com/blog/planning.html", "text": "Planning can turn an ok trip into a fantastic one. Here are all the secrets you might not know.", "tags": "blog" },
+          { "title": "Singapore", "url": "https://www.agirlstravelsecrets.com/blog/singapore.html", "text": "Singapore is a perfect little concentration of everything you love in a small space. You’ll find great food, unbelievable shopping, interesting architecture, wild clubs, and beautiful beaches. You can visit Sentosa Island, Gardens by the Bay and Michelin star restaurants!  " }
+     ]
+
+};
